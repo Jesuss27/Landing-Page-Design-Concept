b39e57a89b36552f0ba186f27881c9968584775f
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-11-17 15:05:17 -0800

    Final Commit

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const navList = document.querySelector(".nav-list")
 
 mobileMenu.addEventListener("click", () => {
     navList.classList.toggle("active");
-    
+    mobileMenu.classList.toggle("is-active");
 
+
+})
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 768){
+        navList.classList.remove("active");
+        mobileMenu.classList.remove("is-active");
+    }
 })
