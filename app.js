b28a22c41d9800075fd2c994868c60828c16c8c7
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-11-17 11:33:56 -0800

    Adding Responsivity

---
+++ app.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.querySelector(".mobile-menu")
+const navList = document.querySelector(".nav-list")
+
+mobileMenu.addEventListener("click", () => {
+    navList.classList.toggle("active");
+    
+
+})
